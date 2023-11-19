c78b4bc5423d1bc187bab2e175e46738cf07d664
Student7751 <145177506+Student7751@users.noreply.github.com>
2023-11-19 16:33:32 +0700

    Добавлены комментарии

--- Application1/pressure.h
+++ Application1/pressure.h
@@ -12,21 +12,27 @@ private:
 	int value = 0;
 public:
 
-	
+	//Объявление геттеров
 	float getHigh();
 	int getValue();
 	DateStruct getDate();
 
+	//Объявление сеттеров
 	void setHigh(float value);
 	void setValue(int val);
 	void setDate(DateStruct date);
 
+	//Объявление функции чтения из файла
 	static Pressure createFromStream(std::istream& in);
+	//Объявление функции вывода данных
 	void print(std::ostream& out);
 
+	//Объявление функции считывания давления
 	void readPressure(std::istream& file, std::vector<Pressure>& pvec);
 
+	//Объявление функции вывода давления
 	void printPressure(std::vector<Pressure>& pvec, std::ostream& out);
 
+	//Объявление функции проверки данных на корректность
 	void CorrectValue(float heigh, int value);
 };
