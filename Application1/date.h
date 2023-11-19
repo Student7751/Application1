503293581699e1f15884c91b03f4fbf5ca4c4f43
Student7751 <145177506+Student7751@users.noreply.github.com>
2023-11-19 16:31:50 +0700

    Добавлены комментарии

--- Application1/date.h
+++ Application1/date.h
@@ -6,25 +6,23 @@ private:
 
 public:
 
+	//Объявление геттеров
 	int getDD();
-
 	int getMM();
-
 	int getYY();
-
+	//Объявление сеттеров
 	void setDD(int value);
-
 	void setMM(int value);
-
 	void setYY(int value);
 
-
+	//Объявление функции чтения даты
 	void readDate(std::string& dateStr);
+	//Объявление функции вывода даты
 	void printDate(std::ostream& out);
-
+	//Объявление функции проверки корректности даты из строки
 	void CorrectDate(std::string& dateStr);
-
+	//Объявление функции определения года на високосноть
 	bool isLeapYear(int year);
-	
+	//Объявление функции проверки даты на корректность
 	void Correct(int dd, int mm, int yy);
 };
